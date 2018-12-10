b4ac499c6e930bf9cfe2d6b8ad9d7198d904b128
FishmanDefender <mlschott343@gmail.com>
2018-12-09 17:14:31 -0700

    Got reading/writing of csv files in 'Ledger' class working

---
+++ ledger.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import csv
+
+class Ledger(object):
+    '''
+    This ledger class holds all transactions to and from a single account.
+    '''
+
+    def __init__(self, iofile):
+        '''
+        Reads in a file passed as an argument in the 'Ledgers' subdirectory then creates the transaction_list.
+        Modifying the transaction_list modifies the ledger.
+        '''
+
+        cwd = os.getcwd()
+        ledger_dir = str(cwd) + '/Ledgers'
+        file_loc = ledger_dir + '/' + str(iofile)
+
+        #TODO Make it create the file if it doesn't exist
+
+        if not (iofile.endswith('.csv')):
+            raise InputError('File is not a csv file!')
+
+        # self.headers = ['Date', ' Account', ' Reference Number', ' Debit', ' Credit', ' Description']
+
+        self.file_loc = file_loc
+        self.transaction_list = []
+
+        with open(file_loc, newline='') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                l = []
+                for x in row:
+                    x = x.strip()
+                    try:
+                        x = float(x)
+                    except:
+                        x = x
+                    l.append(x)
+                self.transaction_list.append(l)
+
+    def write(self):
+        '''
+        Writes out the current ledger to its associated ledger file. This will overwrite previous versions of the ledger.
+        '''
+
+        with open(str(self.file_loc), 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            for row in self.transaction_list:
+                i = 0
+                nrow = []
+                for x in row:
+                    if i == 0:
+                        nrow.append(str(x))
+                        i = 1
+                    else:
+                        nrow.append((' ' + str(x)))
+                writer.writerow(nrow)
--- main.py
+++ main.py
@@ -1,55 +1,13 @@
 #!/usr/bin/env python3
-
 import sys
 import os
-import csv
+from ledger import *
 
-class Ledger(object):
-
-    """
-    This ledger class holds all transactions to and from a single account.
-    """
-
-    def __init__(self, iofile):
-
-        cwd = os.getcwd()
-        ledger_dir = str(cwd) + '/Ledgers'
-        file_loc = ledger_dir + '/' + str(iofile)
-
-        if not (iofile.endswith('.csv')):
-            raise InputError('File is not a csv file!')
-
-        self.headers = ['Date', 'Account', 'Reference Number', 'Debit', 'Credit', 'Description'] #TODO Make this not hard-coded
-
-        self.file_loc = file_loc
-        self.transaction_list = []
-
-        with open(file_loc, newline='') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                l = []
-                for x in row:
-                    x = x.strip()
-                    try:
-                        x = float(x)
-                    except:
-                        x = x
-                    l.append(x)
-                self.transaction_list.append(l)
-
-    def write(self):
-
-        with open(str(self.file_loc), 'w', newline='') as csv_file:
-            writer = csv.writer(csv_file, delimiter=',')
-            i = 0
-            for row in self.transaction_list:
-                if i == 0:
-                    row = list((str(x)) for x in row)
-                else:
-                    row = list((' ' + str(x)) for x in row)
-                i += 1
-                writer.writerow(row)
-
+#########################################################
+#
+#   Description: Main module. This is where all calls to ledgers will be made.
+#
+#########################################################
 
 ledger1 = Ledger('test.csv')
 print(ledger1.transaction_list)
